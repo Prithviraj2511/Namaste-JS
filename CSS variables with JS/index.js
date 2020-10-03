950f5f454d0149e7a1f05ae50fe3240f8eb7f1ee
Prithviraj2511 <prithvirajpatil2511@gmail.com>
2020-10-03 21:46:10 +0530

    CSS variables with JS project completed

---
+++ CSS variables with JS/index.js
@@ -0,0 +1,17 @@
+var allIps=document.querySelectorAll('.choices input');
+
+function performAction() {
+    var suffix=this.dataset.suffix||'';
+    console.log(this.value);
+    
+    if(this.name=='blur'){
+        document.documentElement.style.setProperty(`--${this.name}`,this.value/10+suffix);
+    }
+    else{
+        document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+    }
+}
+
+
+allIps.forEach(ip => ip.addEventListener('change',performAction));
+allIps.forEach(ip => ip.addEventListener('mousemove',performAction));
